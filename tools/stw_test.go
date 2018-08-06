fdfaf8dc1352ba93b2e30da39bb8ebf470cdb19c
viteshan <viteshan@163.com>
2018-08-06 22:29:34 +0800

    add tools support

--- tools/stw_test.go
+++ tools/stw_test.go
@@ -30,3 +30,75 @@ func TestRWLock(t *testing.T) {
 	mutex.Unlock()
 	fmt.Println("----unlock done---------")
 }
+
+func TestCond(t *testing.T) {
+	wait := sync.WaitGroup{}
+	locker := new(sync.Mutex)
+	cond := sync.NewCond(locker)
+
+	for i := 0; i < 3; i++ {
+		go func(i int) {
+			defer wait.Done()
+			wait.Add(1)
+			cond.L.Lock()
+			fmt.Println("Waiting start...")
+			cond.Wait()
+			fmt.Println("Waiting end...")
+			cond.L.Unlock()
+
+			fmt.Println("Goroutine run. Number:", i)
+		}(i)
+	}
+
+	time.Sleep(2e9)
+	cond.L.Lock()
+	//cond.Signal()
+	cond.Broadcast()
+	cond.L.Unlock()
+
+	//time.Sleep(2e9)
+	//cond.L.Lock()
+	//cond.Signal()
+	//cond.L.Unlock()
+	//
+	//time.Sleep(2e9)
+	//cond.L.Lock()
+	//cond.Signal()
+	//cond.L.Unlock()
+
+	wait.Wait()
+}
+
+func TestRwMutex(t *testing.T) {
+	wait := sync.WaitGroup{}
+	locker := new(sync.RWMutex)
+
+	for i := 0; i < 3; i++ {
+		go func(i int) {
+			defer wait.Done()
+			defer locker.RUnlock()
+			wait.Add(1)
+			fmt.Println("RLock start  get...", i)
+			locker.RLock()
+			fmt.Println("RLock got...", i)
+			time.Sleep(4 * time.Second)
+			fmt.Println("RLock end...", i)
+			fmt.Println("Goroutine run. Number:", i)
+		}(i)
+	}
+
+	go func(i int) {
+		defer wait.Done()
+		defer locker.Unlock()
+		wait.Add(1)
+		fmt.Println("WLock start get...", i)
+		locker.Lock()
+		fmt.Println("WLock got...", i)
+		time.Sleep(2 * time.Second)
+		fmt.Println("WLock end...", i)
+		fmt.Println("Goroutine run. Number:", i)
+	}(8)
+	time.Sleep(1 * time.Second)
+
+	wait.Wait()
+}
